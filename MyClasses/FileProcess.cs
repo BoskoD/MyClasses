03c29bf5c890ac435b2cd5f10cb547cdd25fcd3e
Bosko Danilovic <boskodanilovic@outlook.com>
2023-08-08 16:44:11 +0200

    Add project files.

---
+++ MyClasses/FileProcess.cs
@@ -0,0 +1,14 @@
+﻿namespace MyClasses
+{
+    public class FileProcess
+    {
+        public bool FileExists(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException();
+            }
+            return File.Exists(fileName);
+        }
+    }
+}
---
+++ MyClassesTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MyClassesTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
