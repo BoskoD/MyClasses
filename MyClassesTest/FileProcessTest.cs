a3e3ec1589fb9a790e38d5b1967c95072b8f8ccf
Bosko Danilovic <boskodanilovic@outlook.com>
2023-08-08 18:11:50 +0200

    Handling Exceptions using Try...Catch

--- MyClassesTest/FileProcessTest.cs
+++ MyClassesTest/FileProcessTest.cs
@@ -8,19 +8,57 @@ namespace MyClassesTest
         [TestMethod]
         public void FileNameDoesExist()
         {
-            Assert.Inconclusive();
+            // Arrange
+            FileProcess fp = new();
+            string fileName = @"C:\Windows\WindowsUpdate.log";
+            bool fromCall;
+
+            // Act 
+            fromCall = fp.FileExists(fileName);
+
+            // Assert
+            Assert.IsTrue(fromCall);
         }
 
         [TestMethod]
         public void FileNameDoesNotExist()
         {
-            Assert.Inconclusive();
+            // Arrange
+            FileProcess fp = new();
+            string fileName = @"<nonExistingFile>";
+            bool fromCall;
+
+            // Act
+            fromCall = fp.FileExists(fileName);
+
+            // Assert
+            Assert.IsFalse(fromCall);
         }
 
         [TestMethod]
         public void FileNameNullOrEmpty_UsingTryCatch_ShouldThrowArgumentNullException()
         {
-            Assert.Inconclusive();
+            // Arrange
+            FileProcess fp;
+            string fileName = string.Empty;
+            bool fromCall = false;
+
+            try
+            {
+                // Act
+                fp = new();
+
+                fromCall = fp.FileExists(fileName);
+
+                // Assert: Fail as we should not get here
+                Assert.Fail(@"The call to FileExists()
+                              did NOT throw ArgumentNullException as it should.");
+            }
+            catch (ArgumentException)
+            {
+                // Assert: Test was a success
+                Assert.IsFalse(fromCall);
+            }
         }
 
         [TestMethod]
