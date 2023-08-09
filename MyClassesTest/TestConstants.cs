9629673a55eb1e870cdef25c32d7f32c168b2ca6
Bosko Danilovic <boskodanilovic@outlook.com>
2023-08-09 22:07:54 +0200

    Enable .runsettings to hold test params

--- MyClassesTest/TestConstants.cs
+++ MyClassesTest/TestConstants.cs
@@ -3,7 +3,7 @@
     internal class TestConstants
     {
         public const string GOOD_FILE_NAME = @"C:\Windows\WindowsUpdate.log";
-        public const string BAD_FILE_NAME = @"<nonExistingFile>";
+        public const string BAD_FILE_NAME = @"C:\NotExist.txt";
         public const string EMPTY_FILE_FAIL_MESSAGE = "The call to FileExists() did NOT throw ArgumentNullException as it should";
         public const string EMPTY_FILE_MESSAGE = "Checking for an empty file name";
     }
