a199e3a5496849ec37d758d0ce6340a44e24743b
Bosko Danilovic <v-bodani@microsoft.com>
2023-08-13 15:47:07 +0200

    Equality assertion init

---
+++ MyClassesTest/AssertEqualityTest.cs
@@ -0,0 +1,52 @@
+﻿namespace MyClassesTest
+{
+    [TestClass]
+    public  class AssertEqualityTest
+    {
+        [TestMethod]
+        public void AreNumbersEqualTest()
+        {
+            int value1 = 27;
+            int value2 = 27;
+
+            Assert.AreEqual(value1, value2);
+        }
+
+        [TestMethod]
+        public void AreNumbersNotEqualTest()
+        {
+            object value1 = 27;
+            object value2 = 27L;
+
+            Assert.AreNotEqual(value1, value2);
+        }
+
+        [TestMethod]
+        public void AreStringsEqualTest()
+        {
+            string value1 = "Bosko";
+            string value2 = "Bosko";
+
+            Assert.AreEqual(value1, value2);
+        }
+
+        [TestMethod]
+        public void AreStringsEqualCaseInsensitiveTest()
+        {
+            string value1 = "Bosko";
+            string value2 = "bosko";
+
+            Assert.AreEqual(value1, value2, true);
+        }
+
+        [TestMethod]
+        public void AreStringsNotEqualTest()
+        {
+            string value1 = "Bosko";
+            string value2 = "Milica";
+
+            Assert.AreNotEqual(value1, value2, true);
+        }
+
+    }
+}
