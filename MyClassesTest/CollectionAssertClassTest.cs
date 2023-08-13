4e67b07723a07e23e90394e9cc170ead3b53e77f
Bosko Danilovic <v-bodani@microsoft.com>
2023-08-13 16:43:20 +0200

    Added CollectionAssertClassTests

---
+++ MyClassesTest/CollectionAssertClassTest.cs
@@ -0,0 +1,44 @@
+﻿using MyClasses.PersonClasses;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class CollectionAssertClassTest
+    {
+        [TestMethod]
+        public void AreCollectionsEqualTestNumbers()
+        {
+            List<int> expected = new() { 1, 2, 3, 4, 5 };
+            List<int> actual = new() { 1, 2, 3, 4, 5 };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AreCollectionsEqualTest()
+        {
+            PeopleManager pManager = new();
+            List<Person> expected;
+            List<Person> actual;
+
+            actual = pManager.GetPeople();
+            expected = actual;
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AreCollectionsNotEqualTest()
+        {
+            PeopleManager pManager = new();
+            List<Person> expected;
+            List<Person> actual;
+
+            // not all ppl are employees
+            actual = pManager.GetPeople(); 
+            expected = pManager.GetEmployees();
+
+            CollectionAssert.AreNotEqual(expected, actual);
+        }
+    }
+}
